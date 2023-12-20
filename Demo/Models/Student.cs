b995ba51138ec2d6bc7a09165e6dcc4863bb1361
Tungnt <nguyenthanhtunghutechsg@gmail.com>
2023-12-20 07:56:15 +0700

    Create Student File in folder Models

---
+++ Demo/Models/Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Demo.Models
+{
+    public class Student
+    {
+		private int iD;
+
+		public int ID
+		{
+			get { return iD; }
+			set { iD = value; }
+		}
+		private string name;
+
+		public string Name
+		{
+			get { return name; }
+			set { name = value; }
+		}
+
+
+
+	}
+}
