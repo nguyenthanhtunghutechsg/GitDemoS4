8891a42a338124c499ffb73b099b7c0ae011ae37
Tungnt <nguyenthanhtunghutechsg@gmail.com>
2023-12-20 07:57:32 +0700

    Create Lecture.cs in folder Models

---
+++ Demo/Models/Lecture.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Demo.Models
+{
+    public class Lecture
+    {
+		private string email;
+
+		public string Email
+		{
+			get { return email; }
+			set { email = value; }
+		}
+
+	}
+}
